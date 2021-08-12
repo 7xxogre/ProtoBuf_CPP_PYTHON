ce90af86f366acf4bc5eb464c52266d8d7ed32a4
7xxogre <bestech82@naver.com>
2021-08-12 17:43:49 +0900

    change inference function

--- mmdetection/one_image_inference.py
+++ mmdetection/one_image_inference.py
@@ -66,8 +66,8 @@ def get_predict(model, img_path):
     labels = np.concatenate(labels)
     for i, bbox in enumerate(bboxes):
         if bbox[-1] > 0.4:
-            lst = [img_path, labels[i], bbox[-1], labels[i], \
-                    int(bbox[3]) - int(bbox[1]), int(bbox[2]) - int(bbox[0])]
+            lst = [img_path, str(labels[i]),str(bbox[-1])[:8], \
+                str(labels[i]), str(int(bbox[3]) - int(bbox[1])), str(int(bbox[2]) - int(bbox[0]))]
             ret.append(lst)
 
     return ret
