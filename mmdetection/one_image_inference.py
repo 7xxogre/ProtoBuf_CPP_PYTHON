40c0f06b794a750c51b28edb16d921d70625ae9b
7xxogre <bestech82@naver.com>
2021-08-25 19:20:25 +0900

    add get_json function

--- mmdetection/one_image_inference.py
+++ mmdetection/one_image_inference.py
@@ -3,6 +3,8 @@ import torch
 import numpy as np
 from PIL import Image
 import os
+import cv2
+import json
 
 from mmdet.apis import init_detector
 from mmdet.apis import inference_detector
@@ -68,10 +70,71 @@ def get_predict(model, img_path, percentage = 0.4):
             lst = [img_path, str(labels[i]),str(bbox[-1])[:8], str(labels[i]), \
                 str(int(bbox[3]) - int(bbox[1])), str(int(bbox[2]) - int(bbox[0]))]
             str_lst = ','.join(lst)
-
             ret.append(str_lst)
 
     return ret
+
+def get_json(model, img_path, percentage = 0.4):
+    result = inference_detector(model, img_path)
+    
+    if isinstance(result, tuple):
+        bbox_result, segm_result = result
+        if isinstance(segm_result, tuple):
+            segm_result = segm_result[0] 
+    else:
+        bbox_result, segm_result = result, None
+    image_name = os.path.split(img_path)[-1].split('.')[0]
+    bboxes = np.vstack(bbox_result)
+    labels = [
+            np.full(bbox.shape[0], i, dtype=np.int32)
+            for i, bbox in enumerate(bbox_result)
+        ]
+    if segm_result is not None:
+        segs = [segm for segm_list in segm_result for segm in segm_list]
+    else:
+        segs = None
+    labels = np.concatenate(labels)
+    classes = model.CLASSES
+    img_arr = cv2.imread(img_path)
+    ret_json = dict()
+    ret_json["version"] = "4.5.9"
+    ret_json["flags"] = dict()
+    ret_json["shapes"] = list()
+    ret_json["imagePath"] = str(img_path)
+    ret_json["imageData"] = None
+    ret_json["imageHeight"] = int(img_arr.shape[0])
+    ret_json["imageWidth"] = int(img_arr.shape[1])
+    ret = []
+    for i, bbox in enumerate(bboxes):
+        if bbox[-1] > percentage:
+            obj = dict()
+            obj['label'] = str(classes[labels[i]])
+            
+            points_lst = []
+            if segs is not None:
+                
+                mask = segs[i]
+                conto = np.zeros_like(mask, dtype = np.uint8)
+                
+                conto[mask == False] = 255
+            
+                _, imthres = cv2.threshold(conto, 127, 255, cv2.THRESH_BINARY_INV)
+                
+                contour2, _ = cv2.findContours(imthres, cv2.RETR_EXTERNAL, \
+                                                                cv2.CHAIN_APPROX_SIMPLE)
+                
+                for cord in contour2[0]:
+                    points_lst.append([float(cord[0][0]), float(cord[0][1])])
+            obj['points'] = points_lst
+            obj['group_id'] = None
+            obj['shape_type'] = "polygon"
+            obj['flags'] = dict()
+            ret_json['shapes'].append(obj)
+        with open(image_name + ".json", 'w') as f:
+            json.dump(ret_json, f)
+    return ret_json
+
+
 
 if __name__=="__main__":
     
