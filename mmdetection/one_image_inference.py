c98f1d37cb210367ef65a58cc78142700ad19894
7xxogre <bestech82@naver.com>
2021-09-17 04:11:23 +0900

    add nms algorithm in one image inference

--- mmdetection/one_image_inference.py
+++ mmdetection/one_image_inference.py
@@ -9,9 +9,36 @@ import json
 from mmdet.apis import init_detector
 from mmdet.apis import inference_detector
 
+# NMS algorithm for all class
+def non_max_suppresssion_fast(boxes, overlap_thresh = 0.5):
+    if not boxes.size:
+        return np.empty((0,), dtype=int)
+    if boxes.dtype.kind == 'i':
+        boxes = boxes.astype('float')
+    pick = []
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    area = (x2 - x1) * (y2 - y1)
+    idxs = np.argsort(boxes[:, 4])
+    
+    while idxs.size:
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+        w = np.maximum(0, xx2 - xx1)
+        h = np.maximum(0, yy2 - yy1)
+        overlap = (w * h) / area[idxs[:last]]
+        idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlap_thresh)[0])))
 
+    return pick
 
-def save_predict_obj_img(model, img_path, save_path = None, percentage = 0.4):
+def save_predict_obj_img(model, img_path, save_path = None, percentage = 0.4, NMS = False):
     """
         image 경로를 받아 해당 이미지에 대한 모델의 예측을 생성해 object로 추정되는
         crop을 save_path에 저장하는 함수
@@ -26,16 +53,25 @@ def save_predict_obj_img(model, img_path, save_path = None, percentage = 0.4):
             segm_result = segm_result[0]  # ms rcnn
     else:
         bbox_result, segm_result = result, None
+
     img_name = os.path.split(img_path)[-1].split('.')[0]
     labels = [
             np.full(bbox.shape[0], i, dtype=np.int32)
             for i, bbox in enumerate(bbox_result)
         ]
     labels = np.concatenate(labels)
-
     bboxes = np.vstack(bbox_result)
+    
+
+    # 모든 클래스에 nms 알고리즘 적용
+    if NMS == True:
+        pick = non_max_suppresssion_fast(boxes = bboxes, overlap_thresh = 0.5)
+        bboxes = bboxes[pick]
+        labels = labels[pick]
+
     img_origin = mmcv.imread(img_path).astype(np.uint8)
     cnt = 1
+
     for i in range(len(bboxes)):
         if bboxes[i, -1] > percentage:
             im = Image.fromarray(img_origin[int(bboxes[i, 1]) : int(bboxes[i, 3]),int(bboxes[i, 0]) : int(bboxes[i, 2]),:])
@@ -44,7 +80,7 @@ def save_predict_obj_img(model, img_path, save_path = None, percentage = 0.4):
             cnt += 1
     
 
-def get_predict(model, img_path, percentage = 0.4):
+def get_predict(model, img_path, percentage = 0.4, NMS = False):
     """
         이미지 경로를 받아 해당 이미지에 대한 모델의 예측을 생성하여 
         [[class_num, 확률, class_num, bbox 가로, bbox 세로], ...] 의 object별 리스트 리턴
@@ -59,12 +95,21 @@ def get_predict(model, img_path, percentage = 0.4):
         bbox_result, segm_result = result, None
 
     bboxes = np.vstack(bbox_result)
+
     labels = [
             np.full(bbox.shape[0], i, dtype=np.int32)
             for i, bbox in enumerate(bbox_result)
         ]
-    ret = []
     labels = np.concatenate(labels)
+
+    # 모든 클래스에 nms 알고리즘 적용
+    if NMS == True:
+        pick = non_max_suppresssion_fast(boxes = bboxes, overlap_thresh = 0.5)
+        bboxes = bboxes[pick]
+        labels = labels[pick]
+    
+    ret = []
+    
     for i, bbox in enumerate(bboxes):
         if bbox[-1] > percentage:
             lst = [img_path, str(labels[i]),str(bbox[-1])[:8], str(labels[i]), \
@@ -74,7 +119,7 @@ def get_predict(model, img_path, percentage = 0.4):
 
     return ret
 
-def get_json(model, img_path, percentage = 0.4):
+def get_json(model, img_path, percentage = 0.4, NMS = False):
     result = inference_detector(model, img_path)
     
     if isinstance(result, tuple):
@@ -83,7 +128,8 @@ def get_json(model, img_path, percentage = 0.4):
             segm_result = segm_result[0] 
     else:
         bbox_result, segm_result = result, None
-    json_name = img_path.split('.')[0]
+
+    json_name = img_path.split('.jpg')[0]
     bboxes = np.vstack(bbox_result)
     labels = [
             np.full(bbox.shape[0], i, dtype=np.int32)
@@ -93,9 +139,19 @@ def get_json(model, img_path, percentage = 0.4):
         segs = [segm for segm_list in segm_result for segm in segm_list]
     else:
         segs = None
+
     labels = np.concatenate(labels)
     classes = model.CLASSES
     img_arr = cv2.imread(img_path)
+
+    # NMS 알고리즘 적용
+    if NMS == True:
+        pick = non_max_suppresssion_fast(boxes = bboxes, overlap_thresh = 0.5)
+        bboxes = bboxes[pick]
+        labels = labels[pick]
+        segs = [segs[idx] for idx in pick]        
+
+
     ret_json = dict()
     ret_json["version"] = "4.5.9"
     ret_json["flags"] = dict()
@@ -130,9 +186,12 @@ def get_json(model, img_path, percentage = 0.4):
             obj['shape_type'] = "polygon"
             obj['flags'] = dict()
             ret_json['shapes'].append(obj)
-        with open(json_name + ".json", 'w') as f:
-            json.dump(ret_json, f)
+
+    with open(json_name + ".json", 'w') as f:
+        json.dump(ret_json, f)
+
     return ret_json
+
 
 
 
@@ -140,5 +199,11 @@ if __name__=="__main__":
     
     checkpoint_path = "work_dir/epoch_50.pth"
     
-    save_predict_obj_img("test_data\JPEGImages\C010301_20210726_144327400.jpg")
-    get_predict("test_data\JPEGImages\C010301_20210726_144327400.jpg")
+    # save_predict_obj_img("test_data\JPEGImages\C010301_20210726_144327400.jpg")
+    # get_predict("test_data\JPEGImages\C010301_20210726_144327400.jpg")
+
+    from mmcv import Config
+    cfg = Config.fromfile("customized_config.py")
+    model = init_detector(cfg, checkpoint=checkpoint_path)
+
+    get_json(model,"../Original_converted/converted/JPEGImages/155_data.jpg", NMS = True)
